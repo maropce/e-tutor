eb7869f7e68621232bfdc39ddfc32e21ce925ef6
maropce <radosny.one@gmail.com>
2024-10-21 03:36:41 +0200

    Added CORS config

---
+++ src/main/java/pl/maropce/etutor/config/WebConfig.java
@@ -0,0 +1,24 @@
+package pl.maropce.etutor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5500") // Zakładając, że frontend działa na porcie 5500
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
